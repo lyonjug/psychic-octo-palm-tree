f12cfad1463ff90578b91ea36882b0897284b340
mazerty <mazerty@gmail.com>
2021-12-15 21:00:35 +0100

    fix no search results

--- popt/__init__.py
+++ popt/__init__.py
@@ -16,8 +16,8 @@ def fetch(tweet_id: str, bearer_token: str) -> Set[str]:
     retweeters = [retweeter.get("username") for retweeter in session.get(base_url + "tweets/" + tweet_id + "/retweeted_by").json().get("data")]
     print("retweeters:", ", ".join(retweeters))
 
-    quoters_ids = [quoter.get("author_id") for quoter in session.get(base_url + "tweets/search/recent?tweet.fields=author_id&query=url:" + tweet_id).json().get("data")]
-    repliers_ids = [replier.get("author_id") for replier in session.get(base_url + "tweets/search/recent?tweet.fields=author_id&query=conversation_id:" + tweet_id).json().get("data")]
+    quoters_ids = [quoter.get("author_id") for quoter in session.get(base_url + "tweets/search/recent?tweet.fields=author_id&query=url:" + tweet_id).json().get("data", [])]
+    repliers_ids = [replier.get("author_id") for replier in session.get(base_url + "tweets/search/recent?tweet.fields=author_id&query=conversation_id:" + tweet_id).json().get("data", [])]
     qrs = [qr.get("username") for qr in session.get(base_url + "users?ids=" + ",".join(quoters_ids + repliers_ids)).json().get("data")]
     print("quoters and repliers:", ", ".join(qrs))
 
